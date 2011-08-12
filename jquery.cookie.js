dbe85cb8357d044325c859ce578b0b3d56b4ef2f
Tom Dunlap <apocryphalauthor@gmail.com>
2011-08-11 21:08:23 -0400

    Added the zero parameter argument that tests if cookies are enabled. Added my own test page, tom_test.html. Added a minified js file jquery.cookie-min.js.
    Signed-off-by: Tom Dunlap <apocryphalauthor@gmail.com>

--- jquery.cookie.js
+++ jquery.cookie.js
@@ -1,16 +1,33 @@
 /**
  * jQuery Cookie plugin
  *
- * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ * Copyright (c) 2011 Tom Dunlap
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
+ * Modified from jquery.cookie.js by Klaus Hartl (stilbuero.de)
+ * Origenal source:  https://github.com/carhartl/jquery-cookie
+ *
+ * Last modified:  2011-08-11 by Tom Dunlap:  
+ * - Added zero parameter method that checks if cookies are enables
  */
 jQuery.cookie = function (key, value, options) {
-
+	
+	// if no arguments are passed, check and see if cookies are enabled
+	if (arguments.length == 0){
+		// the id is our test value
+		var id = new Date().getTime();
+		
+		// generate a cookie to probe cookie access
+		document.cookie = '__cookieprobe='+id+';path=/';
+		
+		// if the cookie has been set, then we're good
+		return (document.cookie.indexOf(id) !== -1);
+	}
+	
     // key and at least value given, set cookie...
-    if (arguments.length > 1 && String(value) !== "[object Object]") {
+    else if (arguments.length > 1 && String(value) !== "[object Object]") {
         options = jQuery.extend({}, options);
 
         if (value === null || value === undefined) {
@@ -35,7 +52,9 @@ jQuery.cookie = function (key, value, options) {
     }
 
     // key and possibly options given, get cookie...
-    options = value || {};
-    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
-    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+    else{
+		options = value || {};
+		var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+		return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+	}
 };
